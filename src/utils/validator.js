10c1ffb540bdbacab949fff833b019e27ca965b7
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-06 12:27:23 +0530

    API level validation

---
+++ src/utils/validator.js
@@ -0,0 +1,21 @@
+const validator = require('validator');
+
+async function validator(data){
+
+    const mandatoryField = ['firstName', 'emailId', 'password'];
+    const isAllowed = mandatoryField.every((k)=>Object.keys(data).includes(k));
+
+    if(!isAllowed)
+        throw new Error("Field missing");
+    // firstName validate karo
+    if(data.firstName.length < 3 && data.firstName.length > 20)
+        throw new Error("Enter valid FirstName");
+    // email Validate
+    if(!validator.isEmail(data.emailId))
+        throw new Error("Invalid Email");
+    // Strong Password,check  
+    if(!validator.isStrongPassword(data.password))
+        throw new Error("Weak Password");
+}
+
+module.exports = validator;
