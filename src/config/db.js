350178027877e533453b8fcd1a39ecf5701bcbbd
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-06 11:03:20 +0530

    db connection done

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+async function main(){
+    mongoose.connect(process.env.db_Connect_string);
+    console.log(process.env.db_Connect_string);
+}
+
+module.exports = main;
