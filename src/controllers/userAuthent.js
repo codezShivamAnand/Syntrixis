6f23fe14865678543c9b816bf585c7ecb6f38396
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-09 16:50:13 +0530

    /Login and /register done

--- src/controllers/userAuthent.js
+++ src/controllers/userAuthent.js
@@ -1,14 +1,15 @@
-const validator = require('../utils/validator');
+const validateUserData = require('../utils/validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/users');
+const User = require('../models/user');
+const userAuthMiddleware = require("../middleware/Authmiddleware");
 
 const register = async(req,res)=>{
     try{// API Validation
-        validator(req.body);
+        validateUserData(req.body);
         const {firstName, emailId, password} = req.body;
         // hash the password before stroing it in db 
-        req.body.password = bcrypt.hash(password, 10);
+        req.body.password =await bcrypt.hash(password, 10);
 
         const user = await User.create(req.body); // registered 
         // token generate 
@@ -17,6 +18,41 @@ const register = async(req,res)=>{
         res.status(201).send("user registered successfully");
     }
     catch(err){
-        res.send(400).send("Error: "+err);
+        res.status(400).send("Error: "+err);
     }
 }
+
+const login = async(req,res)=>{
+    try{
+
+        const {emailId, password} = req.body;
+        if(!emailId)
+            throw new Error("Invalid Credentials");
+        if(!password)
+            throw new Error("Invalid Credentials");
+        const user = await User.findOne({emailId}); //findOne returns a promise, so user is a Promise 
+        const match = await bcrypt.compare(password, user.password ); // so put await too here 
+        if(!match)
+            throw new Error("Invalid Credentials");
+
+        const token =  jwt.sign({_id:user._id , emailId:emailId},process.env.JWT_KEY,{expiresIn: 60*60});
+        res.cookie('token',token,{maxAge: 60*60*1000}); // 1hr expiry time set, could also use new date method 
+        res.status(200).send("Logged In Succeessfully");
+    }
+    catch(err){
+        res.status(401).send("Error: "+err.message);
+    }
+}
+
+const getProfile = async (req,res)=>{
+    // we not gonna ask user for (emailId or _id ) , current user's profile -> display 
+    try{
+        // userProfile added to req from -> middleware/userAuthMiddleware.js
+        res.send(req.userProfile);
+    }
+    catch(err){
+        res.send("Error "+err.message);
+    }
+}
+
+module.exports = {register, login, getProfile};
