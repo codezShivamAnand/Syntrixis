14052e18ce539f57380442aab4b31f22368b5870
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-09 18:06:00 +0530

    redis integrated

---
+++ src/config/redis.js
@@ -0,0 +1,16 @@
+const { createClient } = require('redis');
+require('dotenv').config();
+
+
+const redisClient = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: 'redis-11943.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 11943
+    }
+});
+module.exports = redisClient;
+
+
+
