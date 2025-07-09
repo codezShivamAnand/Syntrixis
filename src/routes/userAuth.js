77ccd86c256aad6248cc74c8b34f28600cff03a7
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-09 18:08:25 +0530

    enabled routes for /getProfile and /login

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const authRouter = express.Router();
+const {register, login, getProfile, logout} = require('../controllers/userAuthent');
+const userAuthMiddleware = require("../middleware/Authmiddleware");
 
-authRouter.post('./register', register);
-authRouter.post('./login', login);
-authRouter.post('./logout', logout);
-authRouter.get('./getProfile', getProfile);
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.post('/logout',userAuthMiddleware, logout); // userAuthMiddleware verify wether the token is even valid
+authRouter.get('/getProfile', userAuthMiddleware, getProfile);
 
+module.exports=authRouter;
