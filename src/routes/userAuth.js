c85f134f63fbb986c338ac30f9d6cd161d6df64e
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-10 08:46:36 +0530

    added routes for adminRegister

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const authRouter = express.Router();
-const {register, login, getProfile, logout} = require('../controllers/userAuthent');
+const {register, login, getProfile, logout, adminRegister} = require('../controllers/userAuthent');
 const userAuthMiddleware = require("../middleware/Authmiddleware");
+const adminMiddleware = require("../middleware/adminMiddleware");
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout',userAuthMiddleware, logout); // userAuthMiddleware verify wether the token is even valid
 authRouter.get('/getProfile', userAuthMiddleware, getProfile);
+authRouter.post('/admin/register', adminMiddleware, adminRegister);
 
 module.exports=authRouter;
