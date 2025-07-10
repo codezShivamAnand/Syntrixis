08e6ea4d0e849fa8081424d1fdc95ea6cc5547e7
Shivam Anand <proff.shivam17anand@gmail.com>
2025-07-10 08:51:41 +0530

    redis and authRoutes integrated

--- src/index.js
+++ src/index.js
@@ -3,16 +3,36 @@ const app = express();
 require('dotenv').config();
 const main = require("./config/db");
 const User = require("./models/user");
+const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const authRouter = require('./routes/userAuth');
+const redisClient = require('./config/redis');
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/auth', authRouter);
 
-main()
-.then(async ()=>{
-    app.listen(process.env.PORT, ()=>{
-        console.log("Server Listening at Port: "+ process.env.PORT)
-    })
-    main();
-})
-.catch(err=> console.log("Error  Occurred: "+err));
+const initaliseConnection = async (req,res)=>{
+    try{
+
+        await Promise.all([main(), redisClient.connect() ]);
+        console.log("DB connected");
+
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server Listening at Port: "+ process.env.PORT)
+        });
+    }
+    catch(err){
+        console.log("Error "+ err);
+    }
+}
+initaliseConnection();
+
+// main()
+// .then(async ()=>{
+//     app.listen(process.env.PORT, ()=>{
+//         console.log("Server Listening at Port: "+ process.env.PORT)
+//     })
+//     main();
+// })
+// .catch(err=> console.log("Error  Occurred: "+err));
