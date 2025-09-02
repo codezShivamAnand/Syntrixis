ebfa54a83234f760b94e7b519b6647c95cb3d4d9
Shivam Anand <proff.shivam17anand@gmail.com>
2025-09-02 12:39:10 +0530

    modified name of store to stores

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../authSlice';
 
-export const Store = configureStore({
+export const stores = configureStore({
     reducer:{
         auth: authReducer
     }
