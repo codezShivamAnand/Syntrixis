c2b6ba065e3443f9931e937a81a52ce864190ba1
Shivam Anand <proff.shivam17anand@gmail.com>
2025-09-02 14:08:56 +0530

    added checkAuth handler

--- backend/src/routes/userAuth.js
+++ backend/src/routes/userAuth.js
@@ -8,7 +8,18 @@ authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout',userAuthMiddleware, logout); // userAuthMiddleware verify wether the token is even valid
 authRouter.get('/getProfile', userAuthMiddleware, getProfile);
-
+authRouter.get('/check', userAuthMiddleware, (req,res)=>{
+    const reply = {
+        firstName: req.userProfile.firstName,
+        emailId: req.userProfile.emailId,
+        _id:req.userProfile._id
+    }
+    res.status(200).json({
+        user:reply,
+        message:"Valid User",
+        success:true
+    });
+});
 authRouter.post('/admin/register', adminMiddleware, adminRegister);
 
 module.exports=authRouter;
