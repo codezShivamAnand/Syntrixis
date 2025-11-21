dabcbee2fee3833b2c786b7b51bdcd686f81f80c
Shivam Anand <proff.shivam17anand@gmail.com>
2025-11-21 09:11:38 +0530

    wrraped try catch block for /check controller

--- backend/src/routes/userAuth.js
+++ backend/src/routes/userAuth.js
@@ -9,18 +9,26 @@ authRouter.post('/login', login);
 authRouter.post('/logout',userAuthMiddleware, logout); // userAuthMiddleware verify wether the token is even valid
 authRouter.get('/getProfile', userAuthMiddleware, getProfile);
 authRouter.get('/check', userAuthMiddleware, (req,res)=>{
-    console.log("userProfile", userProfile);
-    const reply = {
-        firstName: req.userProfile.firstName,
-        emailId: req.userProfile.emailId,
-        _id:req.userProfile._id,
-        role:req.userProfile.role
+    try{
+        // console.log("userProfile", req.userProfile);
+        const reply = {
+            firstName: req.userProfile.firstName,
+            emailId: req.userProfile.emailId,
+            _id:req.userProfile._id,
+            role:req.userProfile.role
+        }
+        res.status(200).json({
+            user:reply,
+            message:"Valid User",
+            success:true
+        });
+    }catch(error){
+        console.error("checkAuth error:", error);
+        res.status(401).json({
+            message:"Invalid or expired token",
+            success:false
+        })
     }
-    res.status(200).json({
-        user:reply,
-        message:"Valid User",
-        success:true
-    });
 });
 authRouter.post('/admin/register', adminMiddleware, adminRegister);
 
